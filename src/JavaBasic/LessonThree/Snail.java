525835ecabc1c9563c0070a1357075bbb95b89f8
Myaddib <hotabich80@gmail.com>
2022-02-23 14:33:45 +0200

    public class Snail fix bag

--- src/JavaBasic/LessonThree/Snail.java
+++ src/JavaBasic/LessonThree/Snail.java
@@ -5,13 +5,15 @@ import java.util.Scanner;
 public class Snail {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int speedSnailInTheAfternoon  = scanner.nextInt();
-        int speedSnailAtNight  = scanner.nextInt();
+        int speedSnailInTheAfternoon = scanner.nextInt();
+        int speedSnailAtNight = scanner.nextInt();
         int heightTree = scanner.nextInt();
-        if (speedSnailInTheAfternoon>speedSnailAtNight){
-        int numberOfDays = (heightTree-speedSnailInTheAfternoon)/(speedSnailInTheAfternoon-speedSnailAtNight)+1;
-        System.out.println(numberOfDays);
-        }else {
+        if (speedSnailInTheAfternoon >= heightTree) {
+            System.out.println(1);
+        } else if (speedSnailInTheAfternoon > speedSnailAtNight) {
+            int numberOfDays = (heightTree - speedSnailInTheAfternoon) / (speedSnailInTheAfternoon - speedSnailAtNight) + 1;
+            System.out.println(numberOfDays);
+        } else {
             System.out.println("Impossible");
         }
     }
