816823119c7f1823da0f4a29eb80269d03885ba9
Myaddib <hotabich80@gmail.com>
2022-02-23 19:46:12 +0200

    public class Average fix bag

--- src/JavaBasic/LessonThree/Average.java
+++ src/JavaBasic/LessonThree/Average.java
@@ -4,32 +4,44 @@ import java.util.Scanner;
 
 public class Average {
 
-    public static int averageValue() {
+//    public static int averageValue() {
+//        Scanner scanner = new Scanner(System.in);
+//        int count = 0;
+//        int symm = 0;
+//        while (scanner.hasNextInt()) {
+//            count++;
+//            int number = scanner.nextInt();
+//            symm = symm + number;
+//            if (number == 0 && symm == 0) {
+//                return 0;
+//            }
+//            if (number == 0) {
+//                return symm / (count - 1);
+//            }
+//
+//        }
+//        return symm / count;
+//    }
+
+    public static void main(String[] args) {
+//
+//        System.out.println("Test your code here!\n");
+//
+//        // Get a result of your code
+//
+//        System.out.println(averageValue());
+// good code
         Scanner scanner = new Scanner(System.in);
         int count = 0;
         int symm = 0;
-        while (scanner.hasNextInt()) {
+        int number;
+        do {
             count++;
-            int number = scanner.nextInt();
-            symm = symm + number;
-            if (number == 0 && symm == 0) {
-                return 0;
-            }
-            if (number == 0) {
-                return symm / (count - 1);
-            }
+            number = scanner.nextInt();
+            symm += number;
+        }while (number!=0);
+        System.out.println(symm / (count - 1));
 
-        }
-        return symm / count;
-    }
-
-    public static void main(String[] args) {
-
-        System.out.println("Test your code here!\n");
-
-        // Get a result of your code
-
-        System.out.println(averageValue());
     }
 }
 
